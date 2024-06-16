6b94b4e3ef52ca87bad2e5314cac2184f4318247
Oliver <oliver@MSI>
2024-06-16 13:52:13 -0700

    finished unroll function and tests

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,34 @@
 function unroll(squareArray) {
+    const n = squareArray.length;
+    const values = [];
+    let startRow = 0, endRow = n - 1;
+    let startCol = 0, endCol = n - 1;
 
+    while (startRow <= endRow && startCol <= endCol){
+        for (let col = startCol; col <= endCol; col++) {
+            values.push(squareArray[startRow][col]);
+        }
+        startRow++;
+
+        for (let row = startRow; row <= endRow; row++) {
+            values.push(squareArray[row][endCol]);
+        }
+        endCol--;
+
+        if (startRow <= endRow){
+            for (let col = endCol; col >= startCol; col--) {
+                values.push(squareArray[endRow][col]);
+            }
+            endRow--;
+        }
+        if (startCol <= endCol){
+            for (let row = endRow; row >= startRow; row--) {
+                values.push(squareArray[row][startCol]);
+            }
+            startCol++;
+        }
+    }
+    return values;
 }
-
+    
 module.exports = unroll;
--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -1,9 +1,36 @@
 const unroll = require("./unroll");
 
-describe("#unroll", function () {
+const squareArray = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16]
+];
 
+describe("#unroll", function () {
   it("is a function", function () {
     expect(typeof unroll).toEqual("function");
   });
-
 });
+
+describe("test a square matrix of integers", () => {
+  it("should return an empty array for an empty matrix", () => {
+    expect(unroll([])).toEqual([]);
+  });
+  it("should return a single-element array for a 1x1 matrix", () => {
+    expect(unroll([[1]])).toEqual([1]);
+  });
+  it("should have the total number of units in the final array", () => {
+    const unrolled = unroll(squareArray);
+    const expectedLength = squareArray.length * squareArray[0].length;
+    expect(unrolled.length).toEqual(expectedLength);
+  });
+  it("should return the final array correctly", () => {
+    expect(unroll(squareArray)).toEqual([1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]);
+  });
+})
+
+
+
+
+
